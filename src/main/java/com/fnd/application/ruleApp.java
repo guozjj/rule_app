ce48f526caef75ed8a61a86fcd0f071d8ca37f11
gzj <627601010@qq.com>
2022-12-08 10:44:15 +0800

    规则应用

--- src/main/java/com/fnd/application/ruleApp.java
+++ src/main/java/com/fnd/application/ruleApp.java
@@ -1,6 +1,7 @@
 package com.fnd.application;
 
 //import lombok.extern.slf4j.Slf4j;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
@@ -13,6 +14,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories(value = "com.fnd")
 @ComponentScan(basePackages = "com.fnd")
 @EntityScan(basePackages = "com.fnd")
+@MapperScan("com.fnd.mapper")
 public class ruleApp {
 
     public static void main(String[] args) {
